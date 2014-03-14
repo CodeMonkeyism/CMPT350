67341ea5b9cc111a6a3246b8d07ecefb9ba03e9d
DSakura1987 <taoda001@gmail.com>
2014-03-14 13:15:48 -0600

    Add skeleton of combat, engine and room.

---
+++ script/combat.js
@@ -0,0 +1,11 @@
+var combat = {
+    "init": function(enemy, robotGroup){
+        
+    },
+    "doCombat": function(){
+        // loop through attack - defense.
+    },
+    "finishCombat": function(){
+        //show combat result, include loot and loss
+    }
+}
