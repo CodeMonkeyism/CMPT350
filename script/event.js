cb7b6b73ed44b7ddcb2b6c7b1900dfa315c37928
DSakura1987 <taoda001@gmail.com>
2014-03-18 22:29:09 -0600

    event.js now manage explore

---
+++ script/event.js
@@ -0,0 +1,150 @@
+var event = {
+    exploreLevelOne : 1,
+    exploreLevelTwo : 2,
+    exploreLevelThree : 3,
+    exploreLevelFour : 4,
+    exploreLevelFive : 5,
+    exploreLevelSecret : 6,
+    createRandomEvent  : function ( type ) {
+        switch(type){
+            case "space" :
+                return event.createSpaceEvent();
+                break;
+            case "room" :
+                return event.createRoomEvent();
+                break;
+            case "wander" :
+                return event.createShipEvent();
+                break;
+            default :
+                console.log("unknown event type:" + type);
+        }
+    },
+
+    createShipEvent   : function () {
+        console.log("Create a ship event. So what should be on the ship?")
+    },
+
+    createRoomEvent   : function () {
+        console.log("Create a room event. So what should be on the room?")
+    },
+
+    createSpaceEvent  : function () {
+        console.log("Create a space event. So what should be in the space?")
+    },
+
+    explore : function (robotGroup, sceneLevel) {
+        // Determine explore scene.
+        switch(sceneLevel){
+            case 1:
+                var scenes = event.exploreScene.tier1;
+                break;
+            case 2:
+                var scenes = event.exploreScene.tier2;
+                break;
+            case 3:
+                var scenes = event.exploreScene.tier3;
+                break;
+            case 4:
+                var scenes = event.exploreScene.tier4;
+                break;
+            case 5:
+                var scenes = event.exploreScene.tier5;
+                break;
+            case 6:
+                var scenes = event.exploreScene.tier6;
+                break;
+            default:
+                var scenes = null;
+                break;
+        }
+
+        // Null scene will automatically fail.
+        if (scenes == null) {
+            return event.exploreFailed("Your robots fell to a hole, and flied off the plant.");
+        };
+
+        // Random choose a scene.
+        var sceneIndex = Math.floor(Math.random() * scenes.length);
+
+        // If robot group is too weak, you failed.
+        if (!event.isExplorePossible(robotGroup, scenes[sceneIndex])) {
+            return event.exploreFailed("You robots are too weak.");
+        };
+        
+
+        
+        // The count of loss robot.
+        if (Math.random() < scenes[sceneIndex].lossPossibility) {
+            var lossCount = 0;
+        } 
+        else{
+            var lossCount = Math.floor(scenes[sceneIndex].lossCount * Math.random());
+        };
+        if (lossCount >= robotGroup.robots.length) {
+            return event.exploreFailed("Though you explored that zone, all robots are down. You get nothing back.");
+        };
+        // Robot group removed lost robots.
+        var randomGroup = robotGroup.robots.sort(function(a,b){return Math.random()>.5 ? -1 : 1;});
+        var groupAfterExplore = randomGroup.slice(lossCount);
+
+        // Resource you got.
+        var exploreLoot = new Array();
+        for (var i = scenes[sceneIndex].loot.length - 1; i >= 0; i--) {
+            if (scenes[sceneIndex].loot[i].rate > Math.random()) {
+                var tempObject = scenes[sceneIndex].loot[i].resourceName 
+                                 + "=" 
+                                 + scenes[sceneIndex].loot[i].quantity;
+                exploreLoot.push(tempObject);
+            }
+            else {
+                var tempObject = scenes[sceneIndex].loot[i].resourceName 
+                                 + "=0" 
+                exploreLoot.push(tempObject);
+            };
+            
+        };
+        console.log(exploreLoot);
+
+        return {
+            robotReturned : groupAfterExplore,
+            time: scenes[sceneIndex].time,
+            loot : exploreLoot,
+            result : scenes[sceneIndex].results[lossCount]
+        }
+
+    },
+
+    // Check attackPower, defensePower, HP and minGroupDefensePower.
+    // If any fails, explore is failed.
+    isExplorePossible : function(robotGroup, scene) {
+        if (robotGroup.attackPower < scene.requirement.minAttackPower) {
+            return false;
+        };
+
+        if (robotGroup.defensePower < scene.requirement.minDefensePower) {
+            return false;
+        };
+
+        if (robotGroup.HP < scene.requirement.minHP) {
+            return false;
+        };
+
+        if (robotGroup.minDefense < scene.requirement.minGroupDefensePower) {
+            return false;
+        };
+
+        return true;
+    },
+
+    exploreFailed : function(description) {
+        return {
+                robotReturned : [],
+                time : 0;
+                loot : ["res_Power=0",
+                        "res_Metal=0",
+                        "res_Fuel=0"],
+                result : description
+            }
+    },
+}
---
+++ script/scene/exploreScene.js
@@ -0,0 +1,186 @@
+event.exploreScene = {
+    // Tier 1: two scenes
+    tier1 :[ {
+        name : "Unpowered corridor",
+        description : "The corridor is total dark, nothing can be seen.",
+        // The time to explore in second.
+        time : 30,
+        // Possible loss of robots.
+        lossCount : 0,
+        // Possibility of loss.
+        lossPossibility: 0,
+        // Result descriptions
+        results : ["Just a piece of cake!"],
+        // The minimum requirement for robot group.
+        requirement : {
+            minAttackPower : 5,
+            minDefensePower : 5,
+            minHP : 5,
+            minGroupDefensePower : 5,
+            },
+        loot : [
+            {
+                resourceName : "res_Power",
+                quantity : 20,
+                rate : 1
+            },
+            {
+                resourceName : "res_Metal",
+                quantity : 20,
+                rate : 1
+            },
+            {
+                resourceName : "res_Fuel",
+                quantity : 20,
+                rate : 1
+            },
+            ]
+        },
+
+        {
+        name : "Unpowered storage room",
+        description : "The door of the room is closed and hard to open.",
+        // The time to explore in second.
+        time : 45,
+        // Possible loss of robots.
+        lossCount : 1,
+        // Possibility of loss.
+        lossPossibility: 0.01,
+        // Result descriptions
+        results : ["Just a piece of cake!",
+                "One robot is damaged by fallen piece and unable to repair."
+                ],
+        // The minimum requirement for robot group.
+        requirement : {
+            minAttackPower : 12,
+            minDefensePower : 5,
+            minHP : 5,
+            minGroupDefensePower : 8,
+            },
+        loot : [
+            {
+                resourceName : "res_Power",
+                quantity : 60,
+                rate : 1
+            },
+            {
+                resourceName : "res_Metal",
+                quantity : 60,
+                rate : 1
+            },
+            {
+                resourceName : "res_Fuel",
+                quantity : 60,
+                rate : 1
+            },
+            ]
+        },
+    ],
+
+    // Tier 2: Three scenes
+    tier2 :[ {
+        name : "Damaged corridor",
+        description : "The corridor is damaged, you can see sparks.",
+        // The time to explore in second.
+        time : 60,
+        // Possible loss of robots.
+        lossCount : 1,
+        // Possibility of loss.
+        lossPossibility: 0.02,
+        // Result descriptions
+        results : [
+                "Just a piece of cake!",
+                "One robot is damaged by electric spark. Its CPU is burnt."
+            ],
+        // The minimum requirement for robot group.
+        requirement : {
+            minAttackPower : 12,
+            minDefensePower : 5,
+            minHP : 5,
+            minGroupDefensePower : 8,
+            },
+        loot : [
+            {
+                resourceName : "res_Power",
+                quantity : 70,
+                rate : 0.9
+            },
+            {
+                resourceName : "res_Metal",
+                quantity : 70,
+                rate : 1
+            },
+            {
+                resourceName : "res_Fuel",
+                quantity : 70,
+                rate : 0.8
+            },
+            ]
+        },
+
+        {
+        name : "Damaged storage room",
+        description : "The room is damaged and its door opens and closes randomly.",
+        // The time to explore in second.
+        time : 65,
+        // Possible loss of robots.
+        lossCount : 1,
+        // Possibility of loss.
+        lossPossibility: 0.05,
+        results : [
+                "Just a piece of cake!",
+                "One robot is hit by heavy metal fragment."
+            ],
+        // The minimum requirement for robot group.
+        requirement : {
+            minAttackPower : 12,
+            minDefensePower : 5,
+            minHP : 5,
+            minGroupDefensePower : 8,
+            },
+        loot : [
+            {
+                resourceName : "res_Power",
+                quantity : 75,
+                rate : 0.8
+            },
+            {
+                resourceName : "res_Metal",
+                quantity : 75,
+                rate : 1
+            },
+            {
+                resourceName : "res_Fuel",
+                quantity : 75,
+                rate : 1
+            },
+            ]
+        },
+
+        {
+        name : "Ruined sailor room",
+        description : "The room is ruined. You can see bloodstain on the remains.",
+        // The time to explore in second.
+        time : 55,
+        // Possible loss of robots.
+        lossCount : 0,
+        // Possibility of loss.
+        lossPossibility: 0,
+        results : ["Just a piece of cake!"],
+        // The minimum requirement for robot group.
+        requirement : {
+            minAttackPower : 15,
+            minDefensePower : 5,
+            minHP : 5,
+            minGroupDefensePower : 8,
+            },
+        loot : [
+            {
+                resourceName : "res_Metal",
+                quantity : 55,
+                rate : 1
+            },
+            ]
+        },
+    ],
+}
