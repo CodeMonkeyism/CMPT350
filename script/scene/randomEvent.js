b3a09527ccd53ae4656231a5cc6dee211ccedd2b
DSakura1987 <taoda001@gmail.com>
2014-03-19 00:22:54 -0600

    Add ship event

--- script/scene/randomEvent.js
+++ script/scene/randomEvent.js
@@ -17,12 +17,12 @@ Events.random = {
 
         baseEvent.scenes['nothing'] = {
             text: [
-                'vague shapes move, just out of sight.',
+                'Vague shapes move, just out of sight.',
                 'The silence and the darkness surrounded.'
                 ],
             buttons: {
                 'backinside': {
-                    text: 'go back inside',
+                    text: 'Go back inside',
                     nextScene: 'end'
                 }
             }
@@ -31,12 +31,12 @@ Events.random = {
         baseEvent.scenes['stuff'] = {
             reward : { res_Metal : 10, res_Fuel : 10},
             text : [
-                'a broken robot lay on ground, looks old and useless.',
-                'the only choice is disassemble it, maybe can find some thing useful.'
+                'A broken robot lay on ground, looks old and useless.',
+                'The only choice is disassemble it, maybe can find some thing useful.'
             ],
             buttons: {
                 'backinside': {
-                    text: 'go back inside',
+                    text: 'Go back inside',
                     nextScene: 'end'
                 }
             }
@@ -45,7 +45,49 @@ Events.random = {
         return baseEvent;
     },
     shipEvent : function(baseEvent){
-        
+        baseEvent.title = 'Your room rocked for a while';
+        baseEvent.scenes.start.text = ["You heard a strong sound.",
+                                        "Something hit your plant."];
+        baseEvent.scenes.start.notification = 'A spaceship crashed on your plant.';
+        baseEvent.scenes.start.buttons = {
+            'investigate' : {
+                text: 'Investigate remains',
+                nextScene: { 0.3: 'stuff', 1: 'nothing' }
+            },
+            'ignore': {
+                text: 'Ignore it',
+                nextScene: 'end'
+            }
+        }
+
+        baseEvent.scenes['nothing'] = {
+            text: [
+                'The ship exploded before your robot gets there.',
+                'The silence and the darkness surrounded.'
+                ],
+            buttons: {
+                'backinside': {
+                    text: 'Go back inside',
+                    nextScene: 'end'
+                }
+            }
+        }
+
+        baseEvent.scenes['stuff'] = {
+            reward : { res_Metal : 275, res_Fuel : 290},
+            text : [
+                'The ship is in fire, but your robots manage to put it out.',
+                'By disassembling it, maybe can find some thing useful.'
+            ],
+            buttons: {
+                'backinside': {
+                    text: 'Go back inside',
+                    nextScene: 'end'
+                }
+            }
+        }
+
+        return baseEvent;
     },
     unknownEvent : function(baseEvent){
         baseEvent.isAvailable = function(){return false;}; 
