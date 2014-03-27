f3c48ffa9914ce6cdcc3582482d72dcaa52534dc
guoxingze <guoxingze@gmail.com>
2014-03-27 01:10:29 -0600

    1

--- script/test.js
+++ script/test.js
@@ -66,14 +66,20 @@ $(function(){
 			model.add('res_Scrap',1000000);
 			model.add('res_Lube',1000000);
 <<<<<<< HEAD
+<<<<<<< HEAD
 		}else if(command=="scrap"){
 			unlock.unlockBuilding("bld_ScrapHeap");
 =======
+=======
+>>>>>>> origin/master
 		}else if(command=="cloud"){
 			var theEvent = Events.createRandomEvent("cloud");
 			console.log(theEvent);
 			var theScene = 'start';
 			Events.loadEvent(theEvent,theScene);
+<<<<<<< HEAD
+>>>>>>> origin/master
+=======
 >>>>>>> origin/master
 		};
 
